9cbc59078e439e2c4e5cb4d910987028f7894ced
Bayodele Faith <bayodele144@gmail.com>
2025-10-11 06:07:28 +0100

    collectstatic

--- ChurchDatabase/settings.py
+++ ChurchDatabase/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ek^owf(8)&o+h)!qh%(38xwq%x@y)^-z@xfaaq_(-g%^7bk9v#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["sepcam.pythonanywhere.com",'127.0.0.0:8000']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
